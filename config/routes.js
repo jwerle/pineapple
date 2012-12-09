922b0f3782155897121184a15a684f80a3de9668
jwerle <joseph.werle@gmail.com>
2012-12-09 15:17:48 -0500

    hooked up default exmaple controller and routes

--- config/routes.js
+++ config/routes.js
@@ -2,11 +2,14 @@ const API_VERSION = parseInt(pineapple.config.server.config.version);
 
 var router = new pineapple.router.Router()
 
-// Prefix all routes with /v{VERSION}
-router.prefix('v' + API_VERSION);
+// Prefix all routes with /v{VERSION} - uncomment line below to prefix all routes with an API version
+// router.prefix('v' + API_VERSION);
 
 router.useMethods(['GET', 'POST', 'PUT', 'DEL']);
 router.setDefaultAction('index');
+
+router.create('GET', '/', 'pineapple.Api.index');
+router.get('/:resource', 'pineapple.Api'); // defaults to pineapple.Api.index
 
 /**
   Exmaple user routes
