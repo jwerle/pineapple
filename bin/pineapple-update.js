8f4e46df95b005c0b831b937a3bb51bca5700136
jwerle <joseph.werle@gmail.com>
2012-12-12 21:47:55 -0500

    updated logging strategy and pkg version

--- bin/pineapple-update.js
+++ bin/pineapple-update.js
@@ -2,19 +2,42 @@ var exec = require('child_process').exec
 
 module.exports.call = function(){
   process.chdir(PINEAPPLE_PATH);
+
+  pineapple.logger.info("Updating... (This may take a while)");
   exec('./scripts/update', function(error, stdout, stderr){
-    if (error) {
-      console.error(error)
-    }
+    var i, e = false
 
     if (stderr) {
-      console.error(stderr)
+      pineapple.logger.error("Something went wrong!");
+      stderr = stderr.split("\n");
+
+      for (i = 0; i < stderr.length; i++) {
+        pineapple.logger.error(stderr[i]);
+      }
+      
+      e = true;
     }
 
-    console.log(stdout.cyan);
+    stdout = stdout.split("\n");
+
+    for (i = 0; i < stdout.length; i++) {
+      if (!!~ stdout[i].toLowerCase().indexOf('err')) {
+        pineapple.logger.error(stdout[i]);
+      }
+      else {
+        pineapple.logger.info(stdout[i]);
+      }
+    }
+
+    if (e) {
+      pineapple.logger.fail("Update failed!");
+    }
+    else {
+      pineapple.logger.success("Update successful!");
+    }
   });
 };
 
 module.exports.help = function(help){
-  return help("update", "Installs all the necessary dependents for the Pineapple API Service.");
+  return help("update", "Updates all the necessary dependents for Pineapple. (May require sudo)");
 };
