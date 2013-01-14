f881a813936fccd2211dda3115737ee38fe36e11
jwerle <joseph.werle@gmail.com>
2013-01-14 18:19:03 -0500

    finalized tpl Application controller, added better handling of route callbacks and 404s. better chaining for router methods

--- tpl/app/controllers/Application.js
+++ tpl/app/controllers/Application.js
@@ -1,3 +1,15 @@
-module.exports.Application = function Application() {
-  this.name = pineapple.app.name; 
+function Application() {
+  this.routeNotFound = function(req, res, next) {
+    return routeNotFound.call(this, req, res, next);
+  }
+};
+
+Application.prototype.routeNotFound = function(req, res, next) {
+  return routeNotFound.call(this, req, res, next);
+};
+
+function routeNotFound(req, res, next) {
+  this.notImplemented(req, res, next);
 }
+
+module.exports = Application;
