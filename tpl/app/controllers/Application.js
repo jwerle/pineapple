cc74339db1cdc3718f9ab95f39056d67b67b2147
jwerle <joseph.werle@gmail.com>
2013-01-16 10:22:29 -0500

    added .json() method on the base controller for consolidated calls, added cleanse() method to the base Model, better handling of notfound routes

--- tpl/app/controllers/Application.js
+++ tpl/app/controllers/Application.js
@@ -1,15 +1,15 @@
 function Application() {
-  this.routeNotFound = function(req, res, next) {
-    return routeNotFound.call(this, req, res, next);
+  this.routeNotFound = function(next) {
+    return routeNotFound.call(this, next);
   }
 };
 
-Application.prototype.routeNotFound = function(req, res, next) {
-  return routeNotFound.call(this, req, res, next);
+Application.prototype.routeNotFound = function(next) {
+  return routeNotFound.call(this, next);
 };
 
-function routeNotFound(req, res, next) {
-  this.notImplemented(req, res, next);
+function routeNotFound(next) {
+  this.notImplemented(next);
 }
 
 module.exports = Application;
