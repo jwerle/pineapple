32869f3a3136e445f26e751da655ad567b73984d
pulsedemon <msquashic@gmail.com>
2013-01-18 16:01:44 -0500

    Fixes #4

--- tpl/app/controllers/pineapple/Api.js
+++ tpl/app/controllers/pineapple/Api.js
@@ -1,6 +1,6 @@
 module.exports.Api = function Api() {
   this.index = function(req, res, next) {
-    this.json(req, res, pineapple.server.OK, {
+    this.json(pineapple.server.OK, {
       method   : req.method,
       resource : req.url,
       message  : "Api.index() was called."
